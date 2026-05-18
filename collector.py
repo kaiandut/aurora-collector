57ccc31eff7873104b4e6da804113c766105bc62
kaiandut <onputtha@gmail.com>
2026-05-18 06:26:56 +0200

    add health endpoint

--- collector.py
+++ collector.py
@@ -1,15 +1,20 @@
+from fastapi import FastAPI
 import os
 import psycopg2
 import betfairlightweight
 
+app = FastAPI()
+
 print("Aurora Booting...")
 
+# PostgreSQL Connection
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 conn = psycopg2.connect(DATABASE_URL)
 
 print("Postgres Connected")
 
+# Betfair Client
 trading = betfairlightweight.APIClient(
     username=os.getenv("BETFAIR_USERNAME"),
     password=os.getenv("BETFAIR_PASSWORD"),
@@ -18,3 +23,19 @@ trading = betfairlightweight.APIClient(
 )
 
 print("Betfair Client Ready")
+
+
+# ROOT ENDPOINT
+@app.get("/")
+def root():
+    return {
+        "message": "Aurora Ω Online"
+    }
+
+
+# HEALTH ENDPOINT
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
